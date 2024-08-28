9ba5f2af5eba4723f4adc0f2eb1a60dc81e3b3c3
sulumor <romuald.patfoort@gmail.com>
2024-08-28 22:17:04 +0200

    build: setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', './presets/**/*.{js,vue,ts}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('tailwindcss-primeui')]
+}
